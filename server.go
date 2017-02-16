0e1ec669cc8acf31c167e290987644c47b2c4d49
Egor <egortictac@amil.ru>
2017-02-16 16:24:55 +0300

    fin

--- server.go
+++ server.go
@@ -33,7 +33,7 @@ type UserInfo struct {
 
 var store = sessions.NewCookieStore([]byte("nRrHLlHcHH0u7fUz25Hje9m7uJ5SnJzP"))
 
-var mongoUrl = "mongodb://egor_m:qwer1234@ds135029.mlab.com:35029/spareroom"
+var mongoUrl = "mongodb://egor_m:qwer1234@ds153729.mlab.com:53729/spareroom"
 
 var DBname = "spareroom"
 
